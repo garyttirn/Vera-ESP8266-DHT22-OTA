afdf39d702d29c08be077bbaf3f2d73ddd088b09
Kari Tiirikainen <kari.tiirikainen@gmail.com>
2021-07-22 17:54:20 +0300

    Add temperature memory to optimize DeepSleep and tune battery voltage tresholds
     Changes to be committed:
    	modified:   Vera-ESP8266-DHT22-OTA.ino
    	modified:   config.h

--- Vera-ESP8266-DHT22-OTA.ino
+++ Vera-ESP8266-DHT22-OTA.ino
@@ -1,7 +1,7 @@
 /**
- * ESP8266 12F and DHT22 Temperature and Humidity Sersor with Vera integration. 
- * 
- * Partly based on 
+ * ESP8266 12F and DHT22 Temperature and Humidity Sersor with Vera integration.
+ *
+ * Partly based on
  *  https://github.com/Ispep/Hemautomation/blob/master/ESP8266/ESP8266%2012E%20TemperaturSensor/ESP8266-12E-TemperaturSensor.ino
  *
  * To be powerd by 3xAA or 3xAAA NiMH Cells
@@ -15,7 +15,7 @@
 #include <DHT.h>
 
 #define DHTPIN 4     // GPIO4
-#define DHTTYPE DHT22 
+#define DHTTYPE DHT22
 
 //Atomic OTA Updates
 #define ATOMIC_FS_UPDATE
@@ -24,9 +24,10 @@
 // so the RTC data structure should be padded to a 4-byte multiple.
 struct {
   uint32_t crc32=0;   // 4 bytes
-  uint8_t channel=0;  // 1 byte,   5 in total
+  uint8_t channel=0;  // 1 byte, 5 in total
   uint8_t bssid[6]={0}; // 6 bytes, 11 in total
-  uint8_t padding=0;  // 1 byte,  12 in total
+  float old_temp=0;   // 4 bytes, 15
+  uint8_t padding=0;  // 1 byte, 16 in total
 } rtcData;
 
 //Read battery voltage
@@ -36,7 +37,7 @@ ADC_MODE(ADC_VCC);
 
 DHT dht(DHTPIN, DHTTYPE);
 
-String ProgramVersion  = "0.1";
+String ProgramVersion  = "0.2";
 
 float glb_temp=0,glb_hum=0;
 int glb_batterylevel=0;
@@ -45,9 +46,16 @@ long glb_rssi=0;
 WiFiClient client;
 HTTPClient http;
 
-void GoToDeepSleep(int SleepDelay){
+void gotoDeepSleep(int SleepDelay){
   Serial.print("Sleeping " + String(SleepDelay) + " seconds\n");
   ESP.deepSleepInstant(SleepDelay * 1000000,WAKE_RF_DISABLED);
+}
+
+//Update temperature reading into RTC memory
+void updateOldTemp () {
+  rtcData.old_temp = glb_temp;
+  rtcData.crc32 = crc32(((uint8_t*)&rtcData) + 4, sizeof( rtcData ) - 4,0xffffffff);
+  ESP.rtcUserMemoryWrite(0, (uint32_t*)&rtcData, sizeof( rtcData ));
 }
 
 void update_started() {
@@ -67,37 +75,37 @@ void update_error(int err) {
 }
 
 int checkForUpdates() {
-    ESPhttpUpdate.setLedPin(LED_BUILTIN, LOW);
+  ESPhttpUpdate.setLedPin(LED_BUILTIN, LOW);
 
-    // Add optional callback notifiers
-    ESPhttpUpdate.onStart(update_started);
-    ESPhttpUpdate.onEnd(update_finished);
-    ESPhttpUpdate.onProgress(update_progress);
-    ESPhttpUpdate.onError(update_error);
+  // Add optional callback notifiers
+  ESPhttpUpdate.onStart(update_started);
+  ESPhttpUpdate.onEnd(update_finished);
+  ESPhttpUpdate.onProgress(update_progress);
+  ESPhttpUpdate.onError(update_error);
 
-    if (glb_batterylevel < 50) {
-      Serial.print("\nBattery Level too low for OTA update\n");
-      return 1;
+  if (glb_batterylevel < 50) {
+    Serial.print("\nBattery Level too low for OTA update\n");
+    return 1;
     }
 
-    Serial.print("\nChecking for update from " + String(UpdateURL) + ". Current FW version " + String(FWVersion) + "\n");
-    t_httpUpdate_return ret = ESPhttpUpdate.update(client, String(UpdateURL),String(FWVersion));
+  Serial.print("\nChecking for update from " + String(UpdateURL) + ". Current FW version " + String(FWVersion) + "\n");
+  t_httpUpdate_return ret = ESPhttpUpdate.update(client, String(UpdateURL),String(FWVersion));
 
-    switch (ret) {
-      case HTTP_UPDATE_FAILED:
-        Serial.printf("HTTP_UPDATE_FAILD Error (%d): %s\n", ESPhttpUpdate.getLastError(), ESPhttpUpdate.getLastErrorString().c_str());
-        break;
+  switch (ret) {
+    case HTTP_UPDATE_FAILED:
+      Serial.printf("HTTP_UPDATE_FAILD Error (%d): %s\n", ESPhttpUpdate.getLastError(), ESPhttpUpdate.getLastErrorString().c_str());
+      break;
 
-      case HTTP_UPDATE_NO_UPDATES:
-        Serial.println("HTTP_UPDATE_NO_UPDATES");
-        break;
+    case HTTP_UPDATE_NO_UPDATES:
+      Serial.println("HTTP_UPDATE_NO_UPDATES");
+      break;
 
-      case HTTP_UPDATE_OK:
-        Serial.println("HTTP_UPDATE_OK");
-        delay(5000);
-        ESP.restart();
-        break;
-    }
+    case HTTP_UPDATE_OK:
+      Serial.println("HTTP_UPDATE_OK");
+      delay(5000);
+      ESP.restart();
+      break;
+  }
 
   return 0;
 }
@@ -114,23 +122,23 @@ int connect_wifi (){
 
   // Try to read WiFi settings from RTC memory
   bool rtcValid = false;
-  if( ESP.rtcUserMemoryRead( 0, (uint32_t*)&rtcData, sizeof( rtcData ) ) ) {
+  if (ESP.rtcUserMemoryRead(0,(uint32_t*)&rtcData, sizeof(rtcData))) {
     // Calculate the CRC of what we just read from RTC memory, but skip the first 4 bytes as that's the checksum itself.
-    uint32_t crc = crc32(((uint8_t*)&rtcData) + 4, sizeof( rtcData ) - 4,0xffffffff);
-    if( crc == rtcData.crc32 ) {
+    uint32_t crc = crc32(((uint8_t*)&rtcData) + 4,sizeof(rtcData) - 4,0xffffffff);
+    if (crc == rtcData.crc32) {
       rtcValid = true;
-    }
+      }
   }
 
-  if( rtcValid ) {
+  if(rtcValid) {
     // The RTC data was good, make a quick connection
     Serial.printf("[WIFI] ... quick connect, status: %d\nConnecting.", wifiStatus);
     WiFi.begin( WifiSSID, WifiPass, rtcData.channel, rtcData.bssid, true );
-  }
+    }
   else {
     // The RTC data was not valid, so make a regular connection
-    Serial.printf("[WIFI] ... normal connect, status: %d\nConnecting.", wifiStatus);
-    WiFi.begin( WifiSSID, WifiPass );
+    Serial.printf("[WIFI] ... normal connect, status: %d\n", wifiStatus);
+    WiFi.begin( WifiSSID, WifiPass, WifiChannel, WifiBSSID, true );
   }
 
   while( wifiStatus != WL_CONNECTED ) {
@@ -152,7 +160,7 @@ int connect_wifi (){
       WiFi.disconnect( true );
       delay( 1 );
       WiFi.mode( WIFI_OFF );
-      GoToDeepSleep(sleepTimeS);
+      gotoDeepSleep(sleepTimeS);
       return 1; // Not expecting this to be called, the previous call will never return.
     }
     delay( 50 );
@@ -163,14 +171,14 @@ int connect_wifi (){
 
   if( rtcValid == false ) {
     // Write current connection info back to RTC
-   rtcData.channel = WiFi.channel();
-   memcpy( rtcData.bssid, WiFi.BSSID(), 6 ); // Copy 6 bytes of BSSID (AP's MAC address)
-   rtcData.crc32 = crc32(((uint8_t*)&rtcData) + 4, sizeof( rtcData ) - 4,0xffffffff);
-   ESP.rtcUserMemoryWrite( 0, (uint32_t*)&rtcData, sizeof( rtcData ) );
+    rtcData.channel = WiFi.channel();
+    memcpy( rtcData.bssid, WiFi.BSSID(), 6 ); // Copy 6 bytes of BSSID (AP's MAC address)
+    rtcData.crc32 = crc32(((uint8_t*)&rtcData) + 4, sizeof( rtcData ) - 4,0xffffffff);
+    ESP.rtcUserMemoryWrite( 0, (uint32_t*)&rtcData, sizeof( rtcData ) );
 
-   //Check for updates
-   checkForUpdates();
- }
+    //Check for updates
+    checkForUpdates();
+   }
 
 return wifiStatus;
 }
@@ -178,10 +186,15 @@ return wifiStatus;
 int ReadSensor() {
   Serial.print("\nRequesting DHT22 temperature and humidity...\n");
 
+  dht.begin();
+
+  //Allow DHT22 time to refresh
+  delay(2000);
+
   // Reading temperature or humidity takes about 250 milliseconds!
   // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
   // Read temperature as Celsius (the default)
-    
+ 
   glb_temp = dht.readTemperature();
   glb_hum = dht.readHumidity();
 
@@ -197,7 +210,7 @@ int ReadSensor() {
 
 int getBatteryStatus(){
   glb_batterylevel=ESP.getVcc();
-    
+
   Serial.println("Battery VCC: " + String(glb_batterylevel*0.001)+ "V");
 
   //Calculate estimated percentage
@@ -207,16 +220,16 @@ int getBatteryStatus(){
   else if ( glb_batterylevel <= 3799 || glb_batterylevel >= 3600 ) {
     glb_batterylevel = 50;
     }
-  else if ( glb_batterylevel <= 3599 || glb_batterylevel >= 3100 ) {
+  else if ( glb_batterylevel <= 3599 || glb_batterylevel >= 3300 ) {
     glb_batterylevel = 10;
     }
   else {
-    //Battery too low (<3100), sleep 6h or forever which ever comes first.
-    GoToDeepSleep(21600);
+    //Battery too low (<3300), sleep 6h or forever which ever comes first.
+    gotoDeepSleep(21600);
     }
 
   Serial.println("Battery level: " + String(glb_batterylevel) + "%");
-    
+
   return 0; 
 }
 
@@ -224,20 +237,20 @@ int getRSSI(){
   glb_rssi=WiFi.RSSI();
 
   Serial.println("WiFi RSSI: " + String(glb_rssi) + "dBm");
-  
-  return 0; 
+
+  return 0;
 }
 
 int GetHttpURL(String MyURL){
   Serial.print("[HTTP] begin...\n");
 
   http.begin(client,String(VeraBaseURL) + MyURL);
-    
+
   int httpCode = http.GET();
 
    if (httpCode > 0) {
      Serial.printf("[HTTP] GET... code: %d\n", httpCode);
-    
+
      if (httpCode == HTTP_CODE_OK) {
         http.writeToStream(&Serial);
         }
@@ -257,41 +270,43 @@ void setup() {
   //Disable WiFi status LED to save battery
   wifi_status_led_uninstall();
 
-  delay(100);
-
   Serial.begin(115200);
-  //Debug WiFi off
   Serial.setDebugOutput(false);
-
-  dht.begin();
-
-  //Allow DHT22 time to refresh
-  delay(2000);
-
- Serial.print("\n" + String(ESPName) + " started\n");
-  int SensorStatus = ReadSensor(); 
-
-  if (SensorStatus > 0) {
-    GoToDeepSleep(sleepTimeS);
-  }
+  Serial.print("\n" + String(ESPName) + " started\n");
 
   getBatteryStatus();
-  
+
+  int SensorStatus = ReadSensor();
+
+  if (SensorStatus > 0) {
+    gotoDeepSleep(sleepTimeS);
+  }
+
   // Connect WiFi
   connect_wifi();
 
   getRSSI();
 
   http.setReuse(true);
-  
+
   GetHttpURL("data_request?id=variableset&DeviceNum=" + String(VeraTempDeviceID) + "&serviceId=urn:upnp-org:serviceId:TemperatureSensor1&Variable=CurrentTemperature&Value=" + String(glb_temp));
   GetHttpURL("data_request?id=variableset&DeviceNum=" + String(VeraTempDeviceID) + "&serviceId=urn:upnp-org:serviceId:TemperatureSensor1&Variable=CurrentRSSI&Value=" + String(glb_rssi));
   GetHttpURL("data_request?id=variableset&DeviceNum=" + String(VeraTempDeviceID) + "&serviceId=urn:micasaverde-com:serviceId:HaDevice1&Variable=BatteryLevel&Value=" + String(glb_batterylevel));
   GetHttpURL("data_request?id=variableset&DeviceNum=" + String(VeraHumDeviceID) + "&serviceId=urn:micasaverde-com:serviceId:HumiditySensor1&Variable=CurrentLevel&Value=" + String(glb_hum));
 
   http.end();
-  
-  GoToDeepSleep(sleepTimeS);
+
+  //switch radio off to save energy
+  WiFi.mode(WIFI_OFF);
+
+  //Sleep longer if level change is small, there is very little change during night.
+  if (abs(abs(glb_temp) - abs(rtcData.old_temp)) >= tempThreshold) {
+    updateOldTemp();
+    gotoDeepSleep(sleepTimeS);
+  }
+  else {
+    gotoDeepSleep(sleepTimeL);
+  }
 }
 
 void loop() {
